49e4ed6c00d44ae8a81a83f80b3e88170ae6efbd
threatthreat <grezy8007@gmail.com>
2025-03-29 08:48:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '16708960'))
 API_HASH = environ.get('API_HASH', 'dda7630be99593256cb7c520eae0ce6d')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7933550773:AAHaRSIpeoQSCw4XVh1AxiiZ1kSmbo54tso")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7769999187:AAElbFEeuIsOIm39vcFZ3oHTRY0KL-kYCwo")
 
 # ============================
 # Bot Settings Configuration
