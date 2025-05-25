26d51a6406b69f2938d34edebe6a41d720c1d282
threatthreat <grezy8007@gmail.com>
2025-05-25 07:19:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,7 +99,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesandseries36')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MovieByte_7')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movie_Film_series1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Grezy8')
 MOVIE_UPDATE_CHANNEL_LNK = environ.get('MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/movie_Film_series1')
